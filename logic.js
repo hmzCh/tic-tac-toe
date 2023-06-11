85afe48fe4e55f320cf1b2041e1a69334358731f
Hamza <114156862+hmzCh@users.noreply.github.com>
2023-06-11 20:14:15 +0100

    Make the switching of players into its own function & move it into gameModule

--- logic.js
+++ logic.js
@@ -1,5 +1,4 @@
-const gameboardModule = (() => {
-    
+const gameboardModule = (() => {   
     let gameboard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
     let renderGameboard = () => {
@@ -10,29 +9,44 @@ const gameboardModule = (() => {
     }
 
     return {gameboard, renderGameboard}
-
 }) ();
 
 const playerFactory = (name, sign) => {
     return {name, sign}
 }
-  
-const playerOne = playerFactory("Bob", "X")
-const playerTwo = playerFactory("Rob", "O")
-let currentPlayer = playerOne
+
+const gameModule = (() => {
+    let playerOne = playerFactory("Bob", "X");
+    let playerTwo = playerFactory("Rob", "O");
+    let currentPlayer = playerOne;
+
+    let switchPlayer = () => {
+        if (currentPlayer == playerOne) {
+            currentPlayer = playerTwo;
+        } else {
+            currentPlayer = playerOne;
+        }
+    };
+
+    return {
+        playerOne,
+        playerTwo,
+        get currentPlayer() {       //I need a getter function for currentPlayer but not for playerOne or playerTwo because the value of those two don't change during the game but currentPlayer does
+            return currentPlayer;
+        },
+        switchPlayer,
+    };
+})();
+
 
 
 for (let i = 0; i < gameboardModule.gameboard.length; i++) {
     const cell = document.querySelector(`#cell-${i}`);
     cell.addEventListener("click", function() {
         if (gameboardModule.gameboard[i] == " ") {          //Checks that the cell hasn't already been marked
-            gameboardModule.gameboard[i] = currentPlayer.sign
+            gameboardModule.gameboard[i] = gameModule.currentPlayer.sign
             gameboardModule.renderGameboard()
-            if (currentPlayer == playerOne) {               //Switches the player
-                currentPlayer = playerTwo
-            } else {
-                currentPlayer = playerOne
-        }}
-
+            gameModule.switchPlayer()
+        }
     });
 }
